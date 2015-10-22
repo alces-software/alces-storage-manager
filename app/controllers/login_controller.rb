9abc5d7ece9bd2750be8a86656e981544fb0e2ba
James Muscat <james.muscat@alces-software.com>
2015-10-22 14:39:09 +0100

    Simple unstyled login form and controller method.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,8 @@
 class LoginController < ApplicationController
   def index
   end
+  
+  def authenticate
+    render html: "You submitted username <em>%s</em> and password <em>%s</em>.".html_safe % [params[:username], params[:password]]
+  end
 end
