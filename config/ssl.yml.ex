02f4daa419649be8c411ac06f5ce863043cd5adb
James Muscat <james.muscat@alces-software.com>
2015-10-29 16:10:40 +0000

    Remove actual config files and replace with examples.

---
+++ config/ssl.yml.ex
@@ -0,0 +1,4 @@
+:root: /path/to/ssl/client/
+:certificate: daemon-client_crt.pem
+:key: daemon-client_key.pem
+:ca: alces-ca_crt.pem
--- config/targets.yml.ex
+++ config/targets.yml.ex
@@ -4,7 +4,9 @@
     :type: :polymorph
     :dir_spec: :home
     :address: "127.0.0.1:25268"
+    :ssl: true
   Root:
     :type: :polymorph
     :dir:   "/"
     :address: "127.0.0.1:25268"
+    :ssl: true
