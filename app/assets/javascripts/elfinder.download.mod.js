e4296ba1d9e72d04cee8d32c65629c7ba4e106e8
James Muscat <james.muscat@alces-software.com>
2016-01-08 11:01:59 +0000

    Replace comment at top of file.

--- app/assets/javascripts/elfinder.download.mod.js
+++ app/assets/javascripts/elfinder.download.mod.js
@@ -1,11 +1,29 @@
 "use strict";
-/**
- * @class elFinder command "download". 
- * Download selected files.
- * Only for new api
+/* ==============================================================================
+ * Copyright (C) 2015 Stephen F. Norledge and Alces Software Ltd.
  *
- * @author Dmitry (dio) Levashov, dio@std42.ru
- **/
+ * This file/package is part of Alces Storage Manager.
+ *
+ * Alces Storage Manager is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Alces Storage Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this package.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information on the Alces Storage Manager, please visit:
+ * https://github.com/alces-software/alces-storage-manager
+ * 
+ * This file based on elfinder's download command, Dmitry Levashov, dio@std42.ru
+ * 
+ *==============================================================================
+ */
 elFinder.prototype.commands.download = function() {
   var self   = this,
     fm     = this.fm,
