274ad677fc9b3dacb64cbec707f43405ffc3f513
James Muscat <james.muscat@alces-software.com>
2016-08-18 15:08:13 +0100

    Start to list available storage collections.

--- client/src/components/pages/StorageSelectionPage.js
+++ client/src/components/pages/StorageSelectionPage.js
@@ -1,12 +1,43 @@
 
 import React from 'react';
 
+import _ from 'lodash';
+
+import SelectionPage from 'components/SelectionPage';
+import StorageSelectionCard from 'storage/components/StorageSelectionCard';
+
 export default class StorageSelectionPage extends React.Component {
   render() {
     const {
       storage: {hosts},
     } = this.props;
 
-    return <p>I know of {hosts.length} collections</p>
+    const storageAvailableMessage = _.isEmpty(hosts) ?
+      (
+        <strong>
+          There are no storage collections available.
+        </strong>
+      )
+      :
+      `Select a storage collection below to get started.`;
+
+    const header = (
+      <div>
+        <p>
+          {storageAvailableMessage}
+        </p>
+      </div>
+    );
+
+    return (
+      <SelectionPage
+        items={hosts}
+        keyProp="address"
+        header={header}
+        selectionBoxComponent={StorageSelectionCard}
+        selectionBoxProps={null}
+        loggingOut={false}
+      />
+    );
   }
 }
