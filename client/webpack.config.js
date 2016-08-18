dd65c98bd9964408e540c179a3bec4cd8a4cd75c
James Muscat <james.muscat@alces-software.com>
2016-08-18 10:34:17 +0100

    s/access/storage/

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,7 +6,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var env = process.env.NODE_ENV;
 
-var appName = "alces-access-manager";
+var appName = "alces-storage-manager";
 var entries, devServer, devtool, outputFile, pathinfo, plugins, publicPath,
     loaders;
 
