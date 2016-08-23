f722936d78d7d49448a92908df9557a14e04a9c2
James Muscat <james.muscat@alces-software.com>
2016-08-23 16:24:00 +0100

    Store username against storage hosts we've authenticated against.

--- client/src/modules/storage/reducer.js
+++ client/src/modules/storage/reducer.js
@@ -1,4 +1,4 @@
-
+import _ from 'lodash';
 import {resolve} from 'redux-simple-promise';
 
 import * as actionTypes from './actionTypes';
@@ -16,7 +16,27 @@ export default function reducer(state=initialState, action) {
         hosts: action.payload,
       };
 
+    case resolve(actionTypes.AUTHENTICATE):
+      const storageHostAddress = action.meta.payload.storageHost.address;
+      const username = action.meta.payload.username;
+      return {
+        hosts: modifyStorageInState(
+          state.hosts,
+        storageHostAddress,
+        (s) => { s.username = username }
+      )};
+
     default:
       return state;
   }
+}
+
+
+function modifyStorageInState(state, address, modifyFn) {
+  let newState = _.clone(state);
+  const clusterIndex = _.findIndex(newState, ['address', address]);
+  const newCluster = _.clone(newState[clusterIndex]);
+  modifyFn(newCluster);
+  newState[clusterIndex] = newCluster;
+  return newState;
 }
