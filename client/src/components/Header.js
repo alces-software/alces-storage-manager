a247fc332995d40d86e27ed37120ec1b20de29c2
James Muscat <james.muscat@alces-software.com>
2016-08-31 13:27:35 +0100

    Show list of other connected storage hosts.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
-
+import _ from 'lodash';
 import React, {PropTypes} from 'react';
-import { Nav, NavItem } from 'react-bootstrap';
+import { MenuItem, Nav, NavDropdown } from 'react-bootstrap';
 import { Header as FlightHeader, NavItemLink, Icon } from 'flight-common';
 
 class Header extends React.Component {
@@ -19,16 +19,43 @@ class Header extends React.Component {
   }
 
   navbarRight() {
-    const {currentStorage, logout} = this.props;
+    const {currentStorage, storageHosts, logout, redirectTo} = this.props;
     if (currentStorage) {
+      const currentText = <span>Logged in as <strong>{currentStorage.username}</strong> to <strong>{currentStorage.name}</strong> (<em>{currentStorage.ip}:{currentStorage.auth_port}</em>)</span>;
+      const otherStorageHosts = _(storageHosts).reject((host) => host.id == currentStorage.id).value();
+
+      if (otherStorageHosts.length == 0) {
+        return (
+          <Nav pullRight>
+            <NavDropdown title={currentText} id="navbar-right-menu">
+              <MenuItem onClick={() => {logout(currentStorage)}}>
+                Log out of {currentStorage.name} <Icon name="sign-out" />
+              </MenuItem>
+              <MenuItem onClick={() => redirectTo('/')}>Back to main menu</MenuItem>
+            </NavDropdown>
+
+          </Nav>
+        );
+      }
+
       return (
         <Nav pullRight>
-          <NavItem>
-            Logged in as <strong>{currentStorage.username}</strong> to {currentStorage.ip}:{currentStorage.auth_port}&nbsp;
-          </NavItem>
-          <NavItem onClick={() => {logout(currentStorage)}}>
-            Log out <Icon name="sign-out" />
-          </NavItem>
+          <NavDropdown title={currentText} id="navbar-right-menu">
+            <MenuItem header>Also logged in to:</MenuItem>
+            {_(otherStorageHosts).map(
+              (host) => {
+                return (
+                  <MenuItem onClick={() => redirectTo(`/storage/${host.id}/`)}>{host.name} (<em>{host.username}</em>)</MenuItem>
+                );
+              }
+            ).value()}
+            <MenuItem divider />
+            <MenuItem onClick={() => {logout(currentStorage)}}>
+              Log out of {currentStorage.name} <Icon name="sign-out" />
+            </MenuItem>
+            <MenuItem onClick={() => redirectTo('/')}>Back to main menu</MenuItem>
+          </NavDropdown>
+
         </Nav>
       );
     }
