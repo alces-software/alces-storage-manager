d90e962fd127374847b98d0f1abefcbc657487c3
James Muscat <james.muscat@alces-software.com>
2016-09-22 12:49:44 +0100

    Handle daemons going away while interacting with elfinder API.

--- server/app/controllers/api/v1/finder_controller.rb
+++ server/app/controllers/api/v1/finder_controller.rb
@@ -21,8 +21,25 @@
 #==============================================================================
 class Api::V1::FinderController < ElfinderController
   def api
-# Munge the request parameters since ElfinderRailsConnector expects a username there
+    # Munge the request parameters since ElfinderRailsConnector expects a username there
     params[:username] = session[:authentications][params[:id]]
-    super
+
+    collections = AlcesStorageManager::config['collections']
+
+    if !collections.has_key?(params[:id])
+      # This can arise if a collection goes away and is removed from our config
+      # while a user is interacting with the file manager, having already authenticated.
+      render :json => {:error => 'This storage collection is not currently available.'}
+    else
+      super.tap { |resp|
+        if /Could not communicate with/.match(resp[0])
+          # This is matching the text of the exception caught and JSONified in
+          # ElfinderController, thrown by daemon_client/connection.rb:61. Yuck
+          collections.delete(params[:id])
+          AlcesStorageManager.write_config
+        end
+      }
+    end
   end
+
 end
